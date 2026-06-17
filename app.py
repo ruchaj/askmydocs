a82d6cb2d3d570c53ece3fe1989da32d6302dc70
ruchaj <ruchajrocks@gmail.com>
2026-06-17 13:12:37 -0700

    more fix

--- app.py
+++ app.py
@@ -646,7 +646,7 @@ strong, b { color: #111827 !important; }
 [data-testid="stDownloadButton"] button {
     width: 45px;
     height: 45px;
-    min-height: 45px;
+    min-height: 30px;
     padding: 0 !important;
     border-radius: 50% !important;
     background: #ffffff !important;
@@ -656,7 +656,6 @@ strong, b { color: #111827 !important; }
     display: flex !important;
     align-items: center !important;
     justify-content: center !important;
-    transform: translateY(6px);
     transition: border-color 0.2s, box-shadow 0.2s, transform 0.2s !important;
 }
 [data-testid="stDownloadButton"] button:hover {
